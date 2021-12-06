2de910740a279c54203dcd3342c03245deb23ea8
Eva <fanceeva@gmail.com>
2021-12-06 18:30:00 +0200

    First commit

---
+++ html.js
@@ -0,0 +1,21 @@
+// console.log('Test');
+// const answer = +prompt('Сколько тебе лет?', '18');
+// console.log(answer + 5);
+
+// const answer = [];
+
+// answer[0] = prompt('Как ваше имя?', '');
+// answer[1] = prompt('Как вашa фамилия?', '');
+// answer[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answer);
+
+// const up = (str) => str.toUpperCase();
+// let user = 'user';
+// let str = `these ${up(`${user}s`)} are great`;
+// console.log(str);
+
+const up = (str) => str.toUpperCase();
+let user = 'team';
+let str = `Hi! ${up(`${user}s`)} is the best`;
+console.log(str);
